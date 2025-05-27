b90119ca04c868fc9673d467f522c3d13859facc
Tom Sasson <tom.sasson@live.biu.ac.il>
2025-05-27 19:48:49 +0300

    LCS problem

--- LongestSubSequence.py
+++ LongestSubSequence.py
@@ -17,6 +17,29 @@ class Solution:
         #             self.longestCommonSubsequence(text1, text2[1:]))
         
         # dynamic programming
+        # Initialize a 2D DP array with dimensions (len(text1)+1) x (len(text2)+1)
+        # All values are initially set to 0
+        # dp[i][j] will hold the length of the longest common subsequence (LCS)
+        # between text1[i:] and text2[j:]
+        dp = [[0 for j in range(len(text2) + 1)] 
+                 for i in range(len(text1) + 1)]
+
+        # Fill the DP table in a bottom-up manner
+        # Iterate from the end of each string towards the beginning
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    # If characters match, increment LCS length by 1
+                    # and use the value from the diagonally next cell
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    # If characters do not match, take the maximum
+                    # between skipping a character from text1 or from text2
+                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+        # The result is stored in dp[0][0], representing the LCS length
+        # between the full text1 and text2
+        return dp[0][0]
+
 
 def main():
     sol = Solution()
